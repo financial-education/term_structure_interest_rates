6f4a96dd1ed37b3c3e097e35327ae49ea1f8685f
Matt Brigida <mdbrigida@gmail.com>
2024-01-02 13:06:44 -0500

    fixed calc

--- src/pe1.js
+++ src/pe1.js
@@ -2,6 +2,6 @@ function myJsFunction()
 {
     var y1 = parseFloat(document.getElementById('input1').value);
     var y2 = parseFloat(document.getElementById('input2').value);
-    var forward_rate = ((1 + y2) * (1 + y2)) / (1 + y1) - 1;
+    var forward_rate = 100 * (((1 + y2/100) * (1 + y2/100)) / (1 + y1/100) - 1);
     document.getElementById("out1").innerHTML = forward_rate.toFixed(2);
 }
