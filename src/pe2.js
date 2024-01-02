392e89644dcf951da5aff7cc1506dcfe20c74a21
Matt Brigida <mdbrigida@gmail.com>
2024-01-02 14:32:43 -0500

    fix

--- src/pe2.js
+++ src/pe2.js
@@ -1,7 +1,7 @@
 function myJsFunction2()
 {
-    var y1 = parseFloat(document.getElementById('input3').value);
-    var y2 = parseFloat(document.getElementById('input4').value);
-    var two_year_rate = 100 * Math.sqrt((1 + y1/100) * (1 + y2/100)) - 1);
-    document.getElementById("out2").innerHTML = two_year_rate.toFixed(2);
+    var y1 = parseFloat(document.getElementById('input3').value) / 100;
+    var y2 = parseFloat(document.getElementById('input4').value) / 100;
+    var two_year_rate = Math.sqrt((1 + y1) * (1 + y2)) - 1;
+    document.getElementById("out2").innerHTML = (100 * two_year_rate).toFixed(2);
 }
